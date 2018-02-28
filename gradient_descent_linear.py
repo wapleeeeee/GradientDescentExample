d72db840d52c86f9480e915da326b8518ec5f787
wapleeeeee <waple0820@gmail.com>
2018-02-28 21:30:05 +0800

    finish

--- gradient_descent_linear.py
+++ gradient_descent_linear.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # y = ax + b 
 
@@ -30,16 +31,22 @@ def gradient_descent_runner(init_a,init_b,data,learning_rate,num_iterations):
 	b = init_b
 	for i in range(num_iterations):
 		(a,b) = step_gradient_descent(a,b,data,learning_rate)
+		print('step {}: a:{} b:{} error:{}'.format(i,a,b,calc_error(a,b,data)))
 	return (a,b)
 
 def main():
 	data = np.genfromtxt("data.csv", delimiter=",")
-	learning_rate = 0.0001
+	learning_rate = 0.00001
 	init_a = 0
 	init_b = 0
 	num_iterations = 1000
 	print('init a:{} b:{} error:{}'.format(init_a,init_b,calc_error(init_a,init_b,data)))
 	(new_a,new_b) = gradient_descent_runner(init_a,init_b,data,learning_rate,num_iterations)
+	plt.scatter(data[:,0],data[:,1])
+	pt_x = np.arange(20,80,0.1)
+	pt_y = new_a * pt_x + new_b
+	plt.plot(pt_x,pt_y,color='red')
+	plt.show()
 	print('answer a:{} b:{} error:{}'.format(new_a,new_b,calc_error(new_a,new_b,data)))
 
 if __name__ == '__main__':
--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 ## Gradient Descent Example for Linear Regression
 python3下使用梯度下降实现线性回归
+--
+拟合效果
